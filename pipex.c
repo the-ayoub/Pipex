62d8432df34526bebdc89ad38c82fb6a444f1e66
Ayoub <aybelhaj@cbr9s4.42barcelona.com>
2025-03-21 13:20:58 +0100

    add

--- pipex.c
+++ pipex.c
@@ -3,46 +3,137 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aybelhaj <aybelhaj@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/11 15:58:06 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/03/14 16:06:32 by aybelhaj         ###   ########.fr       */
+/*   Created: 2025/03/20 16:47:45 by aybelhaj          #+#    #+#             */
+/*   Updated: 2025/03/21 13:08:58 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-process_child1(char **argv,char **envp)
+void    free_pipex(t_pipex *pipex)
 {
-	t_pipex pp = NULL;
+    int i;
 
-	pp->infile = open(argv[1], O_RDONLY);
+    if(pipex->fd_in >= 0)
+        close(pipex->fd_in);
+    if(pipex->fd_out >= 0)
+        close(pipex->fd_out);
+    i = 0;
+    while(i < pipex->cmd_count)
+    {
+        if (pipex->procs[i].args)
+            free_array(pipex->procs[i].args);
+        i++;
+    }
+    if(pipex->procs)
+        free(pipex->procs);
 }
-process_child2()
-{
-	t_pipex pp = NULL;
 
-	pp->outfile = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, 0777);
+void    close_pipes(t_pipex *pipex)
+{
+    int i;
+    i = 0;
+    while(i < pipex->cmd_count)
+    {
+        close(pipex->procs[i].pipe_fd[0]);
+        close(pipex->procs[i].pipe_fd[1]);
+        i++;
+    }
 }
-int	main(int argc, char **argv, char **envp)
-{
-	t_pipex	pp = NULL;
 
-	if (argc >= 5)
-	{
-		if(pipe(pp->fd) = -1)
-			error();
-		if ((pp->pid1 = fork()) == 0)
-			process_child1();
-		if ((pp->pid1 = fork()) == -1)
-			error();
-		if ((pp->pid2 = fork()) == 0)
-			process_child2();
-		if ((pp->pid2 = fork()) == -1)
-			error();
-		waitpid(pp->pid1);
-		waitpid(pp->pid2);
-	}
-	else
-		error();
+void    create_pipes(t_pipex *pipex)
+{
+    int i;
+    i = 0;
+    while(i < pipex->cmd_count - 1)
+    {
+        if(pipe(pipex->procs[i].pipe_fd) == -1)
+            perror_exit(ERR_PIPE);
+        i++;
+    }
+}
+
+void    launch_pipeline(t_pipex *pipex)
+{
+    int i;
+
+    create_pipes(pipex);
+    if(pipex->here_doc)
+        handle_here_doc(pipex);
+    i = 0;
+    while(i < pipex->cmd_count)
+    {
+        if((pipex->procs[i].pid = fork()) < 0)
+            perror_exit(ERR_FORK);
+        if(pipex->procs[i].pid == 0)
+        {
+            if(i == 0 && !pipex->here_doc)
+                redirect_io(pipex->fd_in,pipex->procs[i].pipe_fd[1]);
+            else if(i == pipex->cmd_count - 1)
+                dup2(pipex->fd_out,STDOUT_FILENO);
+            else
+                redirect_io(pipex->procs[i -1 ].pipe_fd[0],pipex->procs[i].pipe_fd[1]);       
+        }
+        i++;
+        execute_cmd(pipex, i);
+    }
+    close_pipes(pipex);
+}
+
+void init_process(t_pipex *pipex)
+{
+    int i = 0;
+    int cmd_index;
+
+    while (i < pipex->cmd_count)
+    {
+        cmd_index = i + 2 + pipex->here_doc;
+        pipex->procs[i].cmd = pipex->argv[cmd_index];
+        pipex->procs[i].args = ft_split(pipex->argv[cmd_index], ' ');
+        pipex->procs[i].pid = -1;       
+        i++;
+    }
+}
+
+void init_pipex(t_pipex *pipex, int argc, char **argv, char **envp)
+{
+    pipex->argc = argc;
+    pipex->argv = argv;
+    pipex->envp = envp;
+    pipex->here_doc = is_here_doc(argv[1]);
+    if (pipex->here_doc != 0)
+    {
+        pipex->limiter = argv[2];
+        pipex->cmd_count = argc - 4;
+        pipex->fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+        pipex->here_doc = 1;
+    }
+    else
+    {
+        pipex->cmd_count = argc - 3;
+        pipex->fd_in = open(argv[1], O_RDONLY);
+        pipex->fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    }
+    if (pipex->fd_out < 0 || (!pipex->here_doc && pipex->fd_in < 0))
+        perror_exit(ERR_FILE);
+    pipex->procs = malloc(sizeof(t_process) * pipex->cmd_count);
+    if (!pipex->procs)
+        perror_exit(ERR_MEM);
+    init_process(pipex);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    t_pipex pipex;
+    
+    if (argc < 5)
+        error_exit(ERR_ARGS);
+    
+    init_pipex(&pipex, argc, argv, envp);
+    launch_pipeline(&pipex);
+    free_pipex(&pipex);
+    
+    return (0);
 }
--- pipex.h
+++ pipex.h
@@ -3,27 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aybelhaj <aybelhaj@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: aybelhaj <aybelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:57:44 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/03/14 17:38:40 by aybelhaj         ###   ########.fr       */
+/*   Updated: 2025/03/21 10:44:07 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <stdio.h>
-# include <stdlib.h>
+# include "./libft/libft.h"
 # include <unistd.h>
-# include "./libft/libft.h" 
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
 
-typedef struct s_pipex
+# define ERR_ARGS "Error: Invalid number of arguments"
+# define ERR_PIPE "Error: Pipe creation failed"
+# define ERR_FORK "Error: Fork failed"
+# define ERR_FILE "Error: File operation failed"
+# define ERR_CMD "Error: Command not found"
+# define ERR_MEM "Error: Memory allocation failed"
+
+typedef struct s_process 
 {
-	int		fd[2];
-	int		infile;
-	int		outfile;
-	t_pid	pid1;
-	t_pid	pid2;
-}			t_pipex;
+    char    *cmd;       // Comando completo
+    char    **args;     // Argumentos parseados
+    int     pipe_fd[2]; // Pipe para este proceso
+    pid_t   pid;        // PID del proceso
+} t_process;
+
+typedef struct s_pipex 
+{
+    int     argc;      
+    char    **argv;   
+    char    **envp;    
+    int     fd_in;     
+    int     fd_out;   
+    int     here_doc;   // Flag para here_doc
+    char    *limiter;   // Delimitador para here_doc
+    int     cmd_count;  // Número de comandos
+    t_process *procs;   // Array de procesos
+} t_pipex;
+
+// main.c
+int     main(int argc, char **argv, char **envp);
+
+// init
+void    init_pipex(t_pipex *pipex, int argc, char **argv, char **envp);
+void    init_process(t_pipex *pipex);
+
+// execute
+void    launch_pipeline(t_pipex *pipex);
+void    execute_cmd(t_pipex *pipex, int i);
+char    *find_path(char *cmd, char **envp);
+
+// pipes
+void    create_pipes(t_pipex *pipex);
+void    close_pipes(t_pipex *pipex);
+void    redirect_io(int in_fd, int out_fd);
+
+// here_doc (bonus)
+void    handle_here_doc(t_pipex *pipex);
+
+// utils
+void    error_exit(char *msg);
+void    perror_exit(char *msg);
+void    free_pipex(t_pipex *pipex);
+void    free_array(char **array);
 
 #endif
